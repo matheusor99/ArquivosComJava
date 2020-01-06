a9753f4616db7478ffd2f2e9b9a69479fc5822a3
matheusor99 <matheusor99@gmail.com>
2020-01-06 14:14:15 -0300

    alterações para linux

--- LerArquivos/src/application/Program.java
+++ LerArquivos/src/application/Program.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		File file = new File("\\home\\matheus\\Documentos\\in.txt");
+		File file = new File("//home//matheus//Documentos//in.txt");
 		Scanner sc = null;
 		try {
 			sc = new Scanner(file);
