35dcd9461a25a86e07562a4c31a2b60973dce068
matheusor99 <matheusor99@gmail.com>
2020-01-07 09:34:18 -0300

    try-with-resources

--- FileEBufferedReader/src/application/Program.java
+++ FileEBufferedReader/src/application/Program.java
@@ -9,12 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "//home//matheus//Documentos//in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
 			/**
 			 * este line irá ler 1 linha do arquivo, se o arquivo ja tiver no final e n tiver mais linhas
@@ -29,19 +25,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
